2b391bbfe9815754de800082478b1d50e114f629
aforakaiwa <aforakaiwa@gmail.com>
2025-12-13 14:47:37 +0700

    Create exercise1.py

---
+++ Pertemuan-2/exercise1.py
@@ -0,0 +1,6 @@
+panjangBalok = float(input("Lenght (l) : "))
+lebarBalok = float(input("Width (w) : "))
+tinggiBalok = float(input("Height (h) : "))
+
+volume = panjangBalok * lebarBalok * tinggiBalok
+print("Volume = ", str(volume), "m3")
