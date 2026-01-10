2ed81b2763d5470d245208723c6b1cb7431bd594
aforakaiwa <aforakaiwa@gmail.com>
2026-01-10 15:07:42 +0700

    Create exercise2.py

---
+++ Pertemuan-4/exercise2.py
@@ -0,0 +1,15 @@
+temp = ""
+i = 0
+j = 0
+a = int(input("Input Max Value: "))
+for i in range(a, 0, -1):
+    for j in range(0, i, 1):
+        temp = temp + str(i)
+    temp = temp + "\n"
+
+for i in range(2, a+1, 1):
+    for j in range(0, i, 1):
+        temp = temp + str(i)
+    temp = temp + "\n"
+    
+print (temp)
